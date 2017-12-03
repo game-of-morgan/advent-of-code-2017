0c558902c4888b81728e329131abc8e06f442d02
Morgan Wowk <morgan@boldcommerce.com>
2017-12-03 12:11:05 -0600

    day 1 part two solution

--- day-1/day-1/main.m
+++ day-1/day-1/main.m
@@ -17,25 +17,56 @@ char * readSampleInput(){
     return buff;
 }
 
+void part_one(char* input){
+    
+    char*   character;
+    int     sum = 0,
+            previousDigit = -1,
+            firstDigit = *input - '0';
+    
+    for (character = input; *character != '\0'; character++){
+        int characterValue = *character - '0';
+        if(characterValue == previousDigit){
+            sum += characterValue;
+        }
+        if(strlen(character) == 1 && characterValue == firstDigit){
+            sum += characterValue;
+        }
+        previousDigit = characterValue;
+    }
+    
+    printf("Sum total part one: %d\n", sum);
+}
+
+void part_two(char* input){
+    
+    char*       character;
+    const int   NUM_DIGITS = (int)strlen(input),
+                HALF_STEP = NUM_DIGITS / 2;
+    int         sum = 0,
+                i = 0;
+    
+    for (character = input; *character != '\0'; character++){
+        
+        int cv = *character - '0';
+        int oi = i + HALF_STEP - (NUM_DIGITS / 2 <= i?NUM_DIGITS:0);
+        int ocv = input[oi] - '0';
+        
+        if(cv == ocv){
+            sum += cv;
+        }
+        i++;
+    }
+    
+    printf("Sum total part two: %d\n", sum);
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         char* input = readSampleInput();
-        char* character;
-        int sum = 0, previousDigit = -1, firstDigit = *input - '0';
-        
-        for (character = input; *character != '\0'; character++){
-            int characterValue = *character - '0';
-            if(characterValue == previousDigit){
-                sum += characterValue;
-            }
-            if(strlen(character) == 1 && characterValue == firstDigit){
-                sum += characterValue;
-            }
-            previousDigit = characterValue;
-        }
-        
-        printf("The sum total is %d\n\n", sum);
+        part_one(input);
+        part_two(input);
 
         return 0;
         
